3d7cff82e15a7f2335dd69cd9a9d9b218c3bee52
mehmetozanguven <mehmetozanguven@gmail.com>
2021-10-25 22:26:28 +0300

    Add gitignore for oauth2

---
+++ spring-security-oauth2/src/main/java/com/mehmetozanguven/springsecurityoauth2/service/MyOidcService.java
@@ -0,0 +1,10 @@
+package com.mehmetozanguven.springsecurityoauth2.service;
+
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyOidcService extends OidcUserService {
+
+
+}
